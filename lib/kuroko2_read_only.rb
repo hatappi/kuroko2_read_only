d9992444f796df37b2fe7f20aa5332722ad46ddf
yusaku.hatanaka <hata.yusaku.1225@gmail.com>
2017-02-18 20:38:01 +0900

    Add Kuroko2ReadOnly::AccessDenied

--- lib/kuroko2_read_only.rb
+++ lib/kuroko2_read_only.rb
@@ -1,4 +1,6 @@
 require "kuroko2_read_only/version"
+require "kuroko2_read_only/request_handler"
+require "kuroko2_read_only/exceptions"
 
 module Kuroko2ReadOnly
   # Your code goes here...
---
+++ lib/kuroko2_read_only/exceptions.rb
@@ -0,0 +1,3 @@
+module Kuroko2ReadOnly
+  class AccessDenied < StandardError; end
+end
