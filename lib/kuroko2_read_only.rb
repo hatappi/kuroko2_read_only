0e54a925dc444240724b00c51b053ceaaf52f2f1
yusaku.hatanaka <hata.yusaku.1225@gmail.com>
2017-03-01 23:47:15 +0900

    Add engine

--- lib/kuroko2_read_only.rb
+++ lib/kuroko2_read_only.rb
@@ -1,6 +1,7 @@
 require "kuroko2_read_only/version"
 require "kuroko2_read_only/request_handler"
 require "kuroko2_read_only/exceptions"
+require "kuroko2_read_only/engine"
 
 module Kuroko2ReadOnly
   # Your code goes here...
