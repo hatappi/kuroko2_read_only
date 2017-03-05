da5a81af17ec16dc4bdd2e0ac2126d16b7bf6280
yusaku.hatanaka <hata.yusaku.1225@gmail.com>
2017-03-05 23:27:46 +0900

    Add rails_helper and spec_helper

--- spec/kuroko2_read_only_spec.rb
+++ spec/kuroko2_read_only_spec.rb
@@ -4,8 +4,4 @@ describe Kuroko2ReadOnly do
   it 'has a version number' do
     expect(Kuroko2ReadOnly::VERSION).not_to be nil
   end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
-  end
 end
